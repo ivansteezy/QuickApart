c0e135389466d7dda7366766badd604364f4d992
Ivan Ayala <network.9961@gmail.com>
2022-01-30 12:19:06 -0600

    more audio clips, sfx in UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip clipStart, clipExit;
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        audioSource.PlayOneShot(clipStart);
+        Debug.Log("Comenzando!");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        audioSource.PlayOneShot(clipExit);
         Debug.Log("Saliendo del Juego!");
         Application.Quit();
     }
