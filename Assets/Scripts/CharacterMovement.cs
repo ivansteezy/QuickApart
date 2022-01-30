5c40092cdb5967d5b86f7f591c1daa213cd7b3a8
SAUL <54ulxd@gmail.com>
2022-01-29 21:20:24 -0600

    Destroy characters  on shelf hover

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,42 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     public float movementSpeed = 0.5f;
 
     private int randomDirection;
     private Vector2 movementDirection = new Vector2();
     private Vector2 downRight = Vector2.down + Vector2.right;
-    private Vector2 downLeft =  Vector2.down + Vector2.left;
-
-    private DragDrop dragDrop;
+    private Vector2 downLeft = Vector2.down + Vector2.left;
 
     private Rigidbody2D rb;
+    public bool isDraggin = false;
+
+    public BoxCollider2D redShelveBoxCollider;
+    public BoxCollider2D blueShelveBoxCollider;
+
+    private BoxCollider2D boxCollider2d;
+
+    [SerializeField]
+    private AudioClip audioPickUp, audioDrop;
+    private AudioSource audioSource;
+
 
     void Start()
     {
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        audioSource = GetComponent<AudioSource>();
         randomDirection = Random.Range(0, 3);
 
         if (randomDirection == 0) movementDirection = downLeft;
         if (randomDirection == 1) movementDirection = downRight;
         if (randomDirection == 2) movementDirection = Vector2.down;
 
-        dragDrop = GetComponent<DragDrop>();
+        // dragDrop = GetComponent<DragDrop>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        if(dragDrop.isDraggin)
+        if (isDraggin)
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            rb.isKinematic = true;
+            //rb.isKinematic = true;
             transform.Translate(mousePosition);
         }
         else
         {
-            rb.isKinematic = false;
+            // rb.isKinematic = false;
             transform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
         }
     }
@@ -51,5 +62,22 @@ public class CharacterMovement : MonoBehaviour
         Vector2 newMovementDirection = Vector2.Reflect(movementDirection, inNormal);
 
         movementDirection = newMovementDirection;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //audioSource.PlayOneShot(audioPickUp);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        rb.isKinematic = false;
+        isDraggin = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rb.isKinematic = true;
+        isDraggin = true;
     }
 }
--- Assets/Scripts/DropAreaDetector.cs
+++ Assets/Scripts/DropAreaDetector.cs
@@ -1,14 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DropAreaDetector : MonoBehaviour
 {
-    BoxCollider2D collider2d;
-    public BoxCollider2D characterBoxCollider;
+    public BoxCollider2D m_Collider;
+    public GameObject[] characters;
 
     void Start()
     {
-        collider2d = GetComponent<BoxCollider2D>();
+        m_Collider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        col.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        col.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+
+    IEnumerator WipeIfInside()
+    {
+        characters = FindObjectsOfType<CharacterMovement>().Select((e) => { return e.gameObject; }).ToArray();
+        foreach (var character in characters)
+        {
+            var characterCollider = character.GetComponent<BoxCollider2D>();
+            if (characterCollider)
+            {
+                //If the first GameObject's Bounds enters the second GameObject's Bounds, output the message
+                Collider2D[] overlap = Physics2D.OverlapAreaAll(characterCollider.bounds.min, characterCollider.bounds.max);
+                if (overlap.Length > 1 && Input.GetMouseButtonUp(0))
+                {
+                    Destroy(overlap.FirstOrDefault().gameObject);
+                }
+            }
+        }
+        yield return new WaitForSeconds(0.4f);
+    }
+
+    void Update()
+    {
+        StartCoroutine(WipeIfInside());
     }
 }
